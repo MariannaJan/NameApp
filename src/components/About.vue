5960418cbe7ba850d5f6784d248c9fe02329dade
Marianna <janczak.mariannal@gmail.com>
2018-08-04 13:00:48 +0200

    doc

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h1>This is about page</h1>
-        <p>Coś tam cośtam nad świtezi wodą...</p> 
+        <p>
+            This is a smoall demo app showing the basic use of Vue.js framework.
+        </p> 
     </div>    
 </template>
  
